ad02f876b51005611f980201e9a251eacd083db8
Griswald Brooks <griswald.brooks@picknik.ai>
2022-07-15 19:53:17 -0700

    Modify cmake to make config and repackage

--- rosful/src/node.cpp
+++ rosful/src/node.cpp
@@ -1,19 +1,12 @@
-#include "naive/incrementer.hpp"
-
-#include "rclcpp/rclcpp.hpp"
+#include <say-hello/speaker.hpp>
+#include <rclcpp/rclcpp.hpp>
 
 int main(int argc, char **argv) {
-
 	rclcpp::init(argc,argv);
-
 	auto const node = std::make_shared<rclcpp::Node>("incrementer");
-
-	Incrementer incrementer{node, "/in", "/out"};
-
+  auto const say = sh::speaker{"tacos"};
+	RCLCPP_INFO(node->get_logger(), say().c_str());
 	rclcpp::spin(node);
-	
 	rclcpp::shutdown();
-
 	return 0;
-
 }
