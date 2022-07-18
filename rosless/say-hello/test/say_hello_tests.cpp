4b5f9f0c86a064743982d1e8f23953bf1baeebdb
Griswald Brooks <griswald.brooks@picknik.ai>
2022-07-18 12:24:50 -0700

    Add test and update deb

---
+++ rosless/say-hello/test/say_hello_tests.cpp
@@ -0,0 +1,17 @@
+#include "say-hello/speaker.hpp"  // For speaker
+
+#include "gtest/gtest.h"                   // For TEST_*
+
+namespace sh::test_hello {
+
+TEST(Speaker, Decorate) {
+  auto const sayer = speaker{"tacos"};
+  EXPECT_EQ(sayer(), "I want tacos");
+}
+
+}  // namespace sh::test_hello
+
+int main(int argc, char **argv) {
+    testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
