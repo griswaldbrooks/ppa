1fe92b0a90932dc4fe18d3dc77ab5d47ec63cd9d
Griswald Brooks <griswald.brooks@picknik.ai>
2022-07-20 18:21:55 -0700

    lint

--- rosful/src/node.cpp
+++ rosful/src/node.cpp
@@ -1,12 +1,13 @@
-#include <say-hello/speaker.hpp>
 #include <rclcpp/rclcpp.hpp>
 
+#include <say-hello/speaker.hpp>
+
 int main(int argc, char **argv) {
-	rclcpp::init(argc,argv);
-	auto const node = std::make_shared<rclcpp::Node>("speaker");
+  rclcpp::init(argc, argv);
+  auto const node = std::make_shared<rclcpp::Node>("speaker");
   auto const say = sh::speaker{"tacos"};
-	RCLCPP_INFO(node->get_logger(), say().c_str());
-	rclcpp::spin_some(node);
-	rclcpp::shutdown();
-	return 0;
+  RCLCPP_INFO(node->get_logger(), say().c_str());
+  rclcpp::spin_some(node);
+  rclcpp::shutdown();
+  return 0;
 }
